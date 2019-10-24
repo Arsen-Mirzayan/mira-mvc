eb6afaa231ab32d66d6d557a7a6f8e702fbddcaa
Arsen Mirzayan <56628190+Arsen-Mirzayan@users.noreply.github.com>
2019-10-24 10:47:53 +0000

    вынес системы в интерфейсы

--- src/main/java/com/mira/mvc/system/ClassifierCRUDSystem.java
+++ src/main/java/com/mira/mvc/system/ClassifierCRUDSystem.java
@@ -1,10 +1,7 @@
 package com.mira.mvc.system;
 
-import com.mira.jpa2.Orders;
-import com.mira.jpa2.PageRequest;
 import com.mira.jpa2.PageResponse;
 import com.mira.jpa2.data.Classifier;
-import com.mira.jpa2.data.Classifier_;
 import com.mira.jpa2.service.ClassifierService;
 import com.mira.mvc.dto.ClassifierDto;
 
@@ -15,16 +12,15 @@ import com.mira.mvc.dto.ClassifierDto;
  * @param <SERVICE> класс сервиса для работы с сущностью
  * @param <DTO>     класс DTO
  */
-public abstract class ClassifierCRUDSystem<ENTITY extends Classifier, SERVICE extends ClassifierService<ENTITY>, DTO extends ClassifierDto> extends DefaultCRUDSystem<ENTITY, SERVICE, DTO> {
+public interface ClassifierCRUDSystem<ENTITY extends Classifier, SERVICE extends ClassifierService<ENTITY>, DTO extends ClassifierDto>
+    extends DictionaryCRUDSystem<ENTITY, SERVICE, DTO> {
   /**
    * Находит элемент классификатора по коду
    *
    * @param code код
    * @return элемент классификатора
    */
-  public DTO findByCode(String code) {
-    return convert(getDalService().findByCode(code));
-  }
+  DTO findByCode(String code);
 
   /**
    * Поиск по коду и по имени
@@ -35,10 +31,7 @@ public abstract class ClassifierCRUDSystem<ENTITY extends Classifier, SERVICE ex
    * @param pageSize размер страницы
    * @return страница с результатом
    */
-  public PageResponse<DTO> search(String code, String name, long page, long pageSize) {
-    PageRequest<ENTITY> request = new PageRequest<>(page, pageSize, new Orders<>(Classifier_.code));
-    return convert(getDalService().search(code, name, request));
-  }
+  PageResponse<DTO> search(String code, String name, long page, long pageSize);
 
   /**
    * Находит элемент классификатора по коду, если не находит,то создаёт новый
@@ -47,8 +40,5 @@ public abstract class ClassifierCRUDSystem<ENTITY extends Classifier, SERVICE ex
    * @param name имя
    * @return элемент классификатора
    */
-  public DTO findOrCreate(String code, String name) {
-    return convert(getDalService().findOrCreate(code, name));
-  }
-
+  DTO findOrCreate(String code, String name);
 }
