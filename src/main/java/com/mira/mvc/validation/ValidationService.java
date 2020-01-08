ea44656b8ba77dc4c167c2bcda4d75cbd34c7fdd
arsen <arsen@4f0b01c8-7e08-fb40-bb30-9ffef56c6aa7>
2020-01-08 20:46:20 +0000

    Добавил новый механизм валидации в абстрактный сервис

--- src/main/java/com/mira/mvc/validation/ValidationService.java
+++ src/main/java/com/mira/mvc/validation/ValidationService.java
@@ -6,7 +6,6 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Path;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -26,20 +25,6 @@ public class ValidationService {
   }
 
   /**
-   * Заполняет текст сообщения ошибки в соответствии с кодом и аргументами
-   *
-   * @param error ошибка, в которой нужно заполнить текст
-   * @return та же ошибка с заполненым текстом.
-   */
-  private Error fillMessage(Error error) {
-    if (StringUtils.isNotEmpty(error.getCode())) {
-      String message = messageInterpolator.interpolate(error.getCode(), error.getArguments());
-      error.setMessage(message);
-    }
-    return error;
-  }
-
-  /**
    * Добавляет ошибку в список при условии выполнения переданного условия
    *
    * @param errors    список ошибок. Если пустой, то будет создан новый пустой список.
@@ -47,12 +32,12 @@ public class ValidationService {
    * @param error     ошибка
    * @return список ошибок
    */
-  public Errors addErrorIf(Errors errors, Supplier<Boolean> condition, Error error) {
+  public Errors rejectIf(Errors errors, Supplier<Boolean> condition, Error error) {
     if (errors == null) {
       errors = new Errors();
     }
     if (condition.get()) {
-      errors.getErrors().add(error);
+      add(errors, error);
     }
     return errors;
   }
@@ -66,15 +51,29 @@ public class ValidationService {
    * @param messageCode код сообщения
    * @return список ошибок
    */
-  public Errors addErrorIf(Errors errors, Supplier<Boolean> condition, String field, String messageCode) {
+  public Errors rejectIf(Errors errors, Supplier<Boolean> condition, String field, String messageCode) {
     if (errors == null) {
       errors = new Errors();
     }
     if (condition.get()) {
-      errors.getErrors().add(fillMessage(new Error(Placement.FIELD, field, messageCode)));
-      errors.makeCache();
+      add(errors, new Error(Placement.FIELD, field, messageCode));
     }
     return errors;
+  }
+
+  /**
+   * Добавляет ошибку в список, предварительно её подготовив, а именно заполнив текст ошибки на основе кода/шаблона
+   * и аргуметов.
+   *
+   * @param errors список ошибок
+   * @param error  добавляемая ошибка
+   */
+  private void add(Errors errors, Error error) {
+    if (StringUtils.isNotEmpty(error.getCode())) {
+      String message = messageInterpolator.interpolate(error.getCode(), error.getArguments());
+      error.setMessage(message);
+    }
+    errors.add(error);
   }
 
   /**
@@ -86,8 +85,8 @@ public class ValidationService {
    * @param messageCode код сообщения
    * @return список ошибок
    */
-  public Errors addErrorIfEmpty(Errors errors, String value, String field, String messageCode) {
-    return addErrorIf(errors, () -> StringUtils.isEmpty(value), field, messageCode);
+  public Errors rejectIfEmpty(Errors errors, String value, String field, String messageCode) {
+    return rejectIf(errors, () -> StringUtils.isEmpty(value), field, messageCode);
   }
 
   /**
@@ -99,8 +98,8 @@ public class ValidationService {
    * @param messageCode код сообщения
    * @return список ошибок
    */
-  public Errors addErrorIfNull(Errors errors, Object value, String field, String messageCode) {
-    return addErrorIf(errors, () -> value == null, field, messageCode);
+  public Errors rejectIfNull(Errors errors, Object value, String field, String messageCode) {
+    return rejectIf(errors, () -> value == null, field, messageCode);
   }
 
   /**
@@ -115,17 +114,33 @@ public class ValidationService {
     }
   }
 
+  /**
+   * Выполянет проверку через {@link Validator} и возвращает список
+   *
+   * @param bean проверяемый объект
+   * @return список ошибок
+   */
   public Errors validate(Object bean) {
+    return validate(bean, new Errors());
+  }
+
+  /**
+   * Выполянет проверку через {@link Validator} и добавляет список ошибок в переданный список
+   *
+   * @param bean   проверяемый объект
+   * @param errors список ошибок
+   * @return переданный список ошибок с дополнениями
+   */
+  public Errors validate(Object bean, Errors errors) {
     Set<ConstraintViolation<Object>> violations = validator.validate(bean);
-    List<Error> errors = violations.stream()
+    violations.stream()
         .map(violation ->
             new Error(Placement.FIELD
                 , StreamSupport.stream(violation.getPropertyPath().spliterator(), false).map(Path.Node::getName).collect(Collectors.joining("."))
                 , violation.getMessageTemplate()
             ))
-        .map(this::fillMessage)
-        .collect(Collectors.toList());
-    return new Errors(errors);
+        .forEach(error -> add(errors, error));
+    return errors;
   }
 
 }
