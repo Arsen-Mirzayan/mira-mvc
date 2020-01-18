6bcb59a95711fd9f72a224eb3e38a498483add3a
Arsen Mirzayan <56628190+Arsen-Mirzayan@users.noreply.github.com>
2020-01-18 10:31:36 +0000

    Загрузка шаблонов в интерполятор переделана на ленивую загрузку.

--- src/main/java/com/mira/mvc/validation/impl/VelocityMessageInterpolator.java
+++ src/main/java/com/mira/mvc/validation/impl/VelocityMessageInterpolator.java
@@ -1,10 +1,12 @@
 package com.mira.mvc.validation.impl;
 
 import com.mira.mvc.validation.MessageInterpolator;
+import com.mira.mvc.validation.MessageSource;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.resource.loader.StringResourceLoader;
+import org.apache.velocity.runtime.resource.util.StringResource;
 import org.apache.velocity.runtime.resource.util.StringResourceRepository;
 
 import java.io.StringWriter;
@@ -12,28 +14,36 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
+ * <p>
  * Реализация интерполятора сообщений на основе движка {@link VelocityEngine}. В конструкторе принимает список шаблонов,
  * которые кешируется движком. При вызове методов{@code interpolate} сначала ищет сохранённый скопилированный шаблон,
  * если его нет, то создаёт новый по переданному текстовому шаблону.
+ * </p>
+ * <p>
+ * Шаблоны загружаются левниво из MessageSource при первом обращении. После этого шаблон может быть кеширован и изменение
+ * шаблона в {@link MessageSource} не приведёт к изменению скомпилированного шаблона.
+ * </p>
  */
 public class VelocityMessageInterpolator implements MessageInterpolator {
   private static final String ENCODING = "UTF-8";
   private final VelocityEngine engine;
 
   /**
-   * Создаёт новый экземпляр интерполятора. Инициализирует {@link VelocityEngine} и загружает в него переданные шаблоны.
+   * Создаёт новый экземпляр интерполятора. Инициализирует {@link VelocityEngine}, в который шаблоны загружаются из
+   * {@link MessageSource}.
    *
-   * @param templates карта шаблонов. Ключ - имя шаблон, значение - сам шаблон.
+   * @param messageSource источник шаблонов сообщений
    */
-  public VelocityMessageInterpolator(Map<String, String> templates) {
+  public VelocityMessageInterpolator(MessageSource messageSource) {
     this.engine = new VelocityEngine();
     engine.setProperty(Velocity.RESOURCE_LOADER, "string");
     engine.addProperty("string.resource.loader.class", StringResourceLoader.class.getName());
     engine.addProperty("string.resource.loader.repository.static", "false");
+    engine.addProperty("string.resource.loader." + StringResourceLoader.REPOSITORY_CLASS, DynamicStringResourceRepository.class.getName());
     engine.init();
 
-    StringResourceRepository repository = (StringResourceRepository) engine.getApplicationAttribute(StringResourceLoader.REPOSITORY_NAME_DEFAULT);
-    templates.forEach((key, template) -> repository.putStringResource(key, template, ENCODING));
+    DynamicStringResourceRepository repository = (DynamicStringResourceRepository) engine.getApplicationAttribute(StringResourceLoader.REPOSITORY_NAME_DEFAULT);
+    repository.setMessageSource(messageSource);
   }
 
   @Override
@@ -59,4 +69,51 @@ public class VelocityMessageInterpolator implements MessageInterpolator {
     }
     return writer.toString();
   }
+
+  /**
+   * Репозиторий, который получает сообщения из внешнего источника {@link MessageSource}
+   */
+  public static class DynamicStringResourceRepository implements StringResourceRepository {
+    private MessageSource messageSource;
+
+    public MessageSource getMessageSource() {
+      return messageSource;
+    }
+
+    public void setMessageSource(MessageSource messageSource) {
+      this.messageSource = messageSource;
+    }
+
+    @Override
+    public StringResource getStringResource(String name) {
+      String message = messageSource != null ? messageSource.getMessage(name) : null;
+      return message != null ? new StringResource(message, getEncoding()) : null;
+    }
+
+    @Override
+    public void putStringResource(String name, String body) {
+      //do noting
+    }
+
+    @Override
+    public void putStringResource(String name, String body, String encoding) {
+      //do noting
+    }
+
+    @Override
+    public void removeStringResource(String name) {
+      //do noting
+    }
+
+    @Override
+    public void setEncoding(String encoding) {
+      //do noting
+    }
+
+    @Override
+    public String getEncoding() {
+      return ENCODING;
+    }
+  }
+
 }
